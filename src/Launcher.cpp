debae5ad2adc2094bb31e697e6b7b6b40a64af34
StagTheWizard <montgomey.c.anderson@gmail.com>
2016-11-27 13:28:35 +1300

    Setup endless while loop and window creation with allegro in Launcher.cpp/main.

--- src/Launcher.cpp
+++ src/Launcher.cpp
@@ -2,6 +2,25 @@
 // Created by montgomery anderson on 26/11/16.
 //
 
-int main(int argc, char **argv) {
+#include <allegro5/allegro5.h>
 
+
+uint WINDOW_WIDTH = 600;
+uint WINDOW_HEIGHT = 400;
+
+
+void initialise() {
+    al_init();
+    al_install_keyboard();
+
+    al_create_display(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    while(true);
+}
+
+
+int main(int argc, char **argv) {
+    initialise();
+
+    return 0;
 }
